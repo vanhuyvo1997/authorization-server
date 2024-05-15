61bbc8f3d8423363c1c871ea848b3315abd3aacf
DESKTOP-7NKT9DQ\Huy <vanhuyvo1997.hv@gmail.com>
2024-05-15 09:20:29 +0700

    add opidcscope for initial client

--- src/main/java/com/todo/authorizationserver/AuthorizationServerApplication.java
+++ src/main/java/com/todo/authorizationserver/AuthorizationServerApplication.java
@@ -8,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.core.AuthorizationGrantType;
 import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
+import org.springframework.security.oauth2.core.oidc.OidcScopes;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
 import org.springframework.security.oauth2.server.authorization.settings.ClientSettings;
@@ -25,24 +26,20 @@ public class AuthorizationServerApplication {
 	private PasswordEncoder passwordEncoder;
 	@Autowired
 	private RegisteredClientRepository registeredClientRepository;
-	
+
 	@PostConstruct
 	public void initStartupClient() {
-		RegisteredClient mytaskClient = RegisteredClient
-				.withId(UUID.randomUUID().toString())
-				.clientId("mytask-client")
+		RegisteredClient mytaskClient = RegisteredClient.withId(UUID.randomUUID().toString()).clientId("mytask-client")
 				.clientSecret(passwordEncoder.encode("this-is-secret"))
 				.clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
 				.authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
 				.authorizationGrantType(AuthorizationGrantType.REFRESH_TOKEN)
 				.redirectUri("http://127.0.0.1:8080/login/oauth2/code/oidc-client")
 				.postLogoutRedirectUri("http://127.0.0.1:8080/")
-				.scope("read")
-				.scope("write")
-				.clientSettings(ClientSettings.builder()
-						.requireAuthorizationConsent(true)
-						.requireProofKey(true)
-						.build())
+				.scope("read").scope("write")
+				.scope(OidcScopes.OPENID).scope(OidcScopes.PROFILE)
+				.clientSettings(
+						ClientSettings.builder().requireAuthorizationConsent(true).requireProofKey(true).build())
 				.build();
 		registeredClientRepository.save(mytaskClient);
 	}
